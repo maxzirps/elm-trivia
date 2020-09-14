a7e36b9163d852d095122e35dcb9ba2d8c456c3f
Maximilian Zirps <29249513+zirmax@users.noreply.github.com>
2020-09-14 17:55:12 +0200

    display all questions and their answers

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,11 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text, div, pre)
+import Html exposing (Html, text, div, pre, button)
 import Html.Attributes exposing (style)
 import Http
-import Json.Decode as JD exposing (Decoder, field, string, list)
-import List exposing (map)
+import Json.Decode as JD exposing (Decoder, field, string, list, map6)
+import List exposing (map, append)
 import String exposing (join)
 
 
@@ -33,18 +33,18 @@ type Model
 
 type alias Question =
     { 
-      -- category : String,
-    -- difficulty: String,
-    question: String
-    -- correct_answer: String,
-    -- incorrect_answers: List String
+      category : String,
+    difficulty: String,
+    question: String,
+    correct_answer: String,
+    incorrect_answers: List String
     }
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
   ( Loading
-  , fetchQuestion
+  , fetchQuestions
   )
 
 
@@ -96,28 +96,38 @@ view model =
 
     Success questions ->
       div [style "text-align" "center", style "margin-top" "10rem"] 
-      [ pre [] [text (concatQuestions questions)] ]
+      [ div [] [createHTMLElementsForQuestions questions] ]
 
 
 
 -- HTTP
 
-fetchQuestion : Cmd Msg
-fetchQuestion = Http.get
+fetchQuestions : Cmd Msg
+fetchQuestions = Http.get
       { url = "https://opentdb.com/api.php?amount=10&type=multiple"
       , expect = Http.expectJson GotText questionsDecoder
       }
 
-concatQuestions: (List Question) -> String
-concatQuestions questions = 
-  join "\n\n" (map (\x -> x.question) questions)
+createHTMLElementsForQuestions: (List Question) -> Html msg
+createHTMLElementsForQuestions questions = 
+  div [] (map (\x -> 
+  div[style "margin" "2rem", style "padding" "2rem" ] [ 
+    div [] [ text x.question ], 
+    div [] ((button [style "margin" "0.3rem  "] [text x.correct_answer]) :: (map (\y -> button [style "margin" "0.3rem  "] [text y]) x.incorrect_answers))
+  ]
+   ) questions)
 
 
 questionDecoder: Decoder Question
 questionDecoder = 
-  JD.map Question
+  JD.map5 Question
+    (field "category" string)
+    (field "difficulty" string)
     (field "question" string)
+    (field "correct_answer" string)
+    (field "incorrect_answers" (JD.list string))
 
+ 
 questionsDecoder: Decoder (List Question)
 questionsDecoder = 
   field "results" (JD.list questionDecoder) 
